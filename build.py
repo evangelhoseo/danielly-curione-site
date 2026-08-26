9b270b634134db79fc34194c08fa8e27800e6140
evangelhoseo <flavio.evangelho@gmail.com>
2026-08-26 19:17:26 -0300

    Consent Mode v2 (analytics_storage default denied) - raiz

--- build.py
+++ build.py
@@ -202,7 +202,18 @@ def jsonld(rota):
 def gtm_head():
     if not GTM_ID:
         return ''
-    return ('''<!-- Google Tag Manager -->
+    return ('''<!-- Consent Mode (default: negado ate o aceite no cookie-banner.js) -->
+<script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('consent','default',{
+  'ad_storage':'denied',
+  'ad_user_data':'denied',
+  'ad_personalization':'denied',
+  'analytics_storage':'denied'
+});
+</script>
+<!-- Google Tag Manager -->
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
